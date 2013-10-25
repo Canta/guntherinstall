97654626942fb67eed3bf44e3da50816fd8e5bc3
Daniel Cantarín <omega_canta@yahoo.com>
2013-10-25 19:57:24 -0300

    First installer version for Ubuntu. Should also work on any other debian.

--- install-ubuntu.py
+++ install-ubuntu.py
@@ -7,11 +7,16 @@ import sys, getopt
 import urllib
 import zipfile
 
+if os.geteuid() != 0:
+    exit("Tenés que ejecutar este comando con sudo. Es decir, con permisos de root. Sino no puede instalar los componentes de Günther.\nImposible continuar.")
+
+#FUNCIONES
+
 def CreateInstallDir():
     iDir = ""
     iDir = "/usr/local/gunther"
     if not os.path.exists(iDir): 
-        os.system("sudo mkdir augo+rw " + iDir + " && sudo chmod augo+rw "+iDir)
+        os.system("mkdir augo+rw " + iDir + " && chmod augo+rw "+iDir)
     return iDir
 
 
@@ -38,6 +43,41 @@ def get_desktop_path():
     else:
         return None
 
+def query_si_no(question, default="no"):
+    """Ask a yes/no question via raw_input() and return their answer.
+
+    "question" is a string that is presented to the user.
+    "default" is the presumed answer if the user just hits <Enter>.
+        It must be "yes" (the default), "no" or None (meaning
+        an answer is required of the user).
+
+    The "answer" return value is one of "yes" or "no".
+    """
+    valid = {"si":True,   "s":True,
+             "no":False,     "n":False}
+    if default == None:
+        prompt = " [s/n] "
+    elif default == "yes":
+        prompt = " [S/n] "
+    elif default == "no":
+        prompt = " [s/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+
+    while True:
+        sys.stdout.write(question + prompt)
+        choice = raw_input().lower()
+        if default is not None and choice == '':
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write("Por favor, responda 'si' o 'no' "\
+                             "(o 's' o 'n' en su defecto).\n")
+
+
+#PROGRAMA
+
 # Primero, chequeo las dependencias.
 iDir = None
 PythonPath = os.path.dirname(sys.executable)
@@ -46,8 +86,8 @@ try:
     import setuptools
 except:
     print u"No se encontró el módulo setuptools. Es necesario descargarlo e instalarlo."
-    print "sudo apt-get install python-setuptools"
-    ret = os.system("sudo apt-get install python-setuptools")
+    print "apt-get -y install python-setuptools"
+    ret = os.system("apt-get -y install python-setuptools")
     if ret != 0:
         print u"Ocurrió un error durante la instalación de setuptools."
         exit(ret)
@@ -56,8 +96,8 @@ try:
     import pip
 except:
     print u"No se encontró el módulo pip. Es necesario descargarlo e instalarlo."
-    print "sudo apt-get install python-setuptools"
-    ret = os.system("sudo apt-get install python-pip")
+    print "apt-get -y install python-pip"
+    ret = os.system("apt-get -y install python-pip")
     if ret != 0:
         print u"Ocurrió un error durante la instalación de setuptools."
         exit(ret)
@@ -66,13 +106,13 @@ try:
     import git
 except:
     print u"No se encontró el módulo gitpython. Es necesario descargarlo e instalarlo."
-    print "sudo apt-get install git"
-    ret = os.system("sudo apt-get install git")
+    print "apt-get -y install git"
+    ret = os.system("apt-get -y install git")
     if ret != 0:
         print u"Ocurrió un error durante la instalación de git."
         exit(ret)
-    print "sudo pip install gitpython"
-    ret = os.system("sudo pip install gitpython")
+    print "pip install gitpython"
+    ret = os.system("pip install gitpython")
     if ret != 0:
         print u"Ocurrió un error durante la instalación de gitpython."
         exit(ret)
@@ -81,10 +121,10 @@ try:
     import lxml.etree
 except:
     print u"No se encontró el módulo lxml. Es necesario descargarlo e instalarlo."
-    print "sudo apt-get install python-lxml"
-    os.system("sudo apt-get install python-lxml")
-    print "sudo easy_install --allow-hosts=lxml.de,*.python.org lxml==2.3"
-    ret = os.system("sudo easy_install --allow-hosts=lxml.de,*.python.org lxml==2.3")
+    print "apt-get -y install python-lxml"
+    os.system("apt-get -y install python-lxml")
+    print "easy_install --allow-hosts=lxml.de,*.python.org lxml==2.3"
+    ret = os.system("easy_install --allow-hosts=lxml.de,*.python.org lxml==2.3")
     if ret != 0:
         print u"Ocurrió un error durante la instalación de lxml."
         exit(ret)
@@ -93,8 +133,8 @@ try:
     import numpy
 except:
     print u"No se encontró el módulo numpy. Es necesario descargarlo e instalarlo."
-    print "sudo easy_install numpy"
-    ret = os.system("sudo easy_install numpy")
+    print "pip install numpy"
+    ret = os.system("pip install numpy")
         
     if ret != 0:
         print u"Ocurrió un error durante la instalación de numpy."
@@ -104,8 +144,8 @@ try:
     import PyQt4
 except:
     print u"No se encontró el módulo PyQt4. Es necesario descargarlo e instalarlo."
-    print "sudo apt-get install python-qt4"
-    ret = os.system("sudo apt-get install python-qt4")
+    print "apt-get -y install python-qt4"
+    ret = os.system("apt-get -y install python-qt4")
     if ret != 0:
         print u"Ocurrió un error durante la instalación de PyQt4."
         exit(ret)
@@ -114,8 +154,8 @@ try:
     import pyaudio
 except:
     print u"No se encontró el módulo pyaudio. Es necesario descargarlo e instalarlo."
-    print "sudo pip install pyaudio"
-    ret = os.system("sudo pip install pyaudio")
+    print "pip install pyaudio"
+    ret = os.system("pip install pyaudio")
     if ret != 0:
         print u"Ocurrió un error durante la instalación de pyaudio."
         exit(ret)
@@ -124,8 +164,8 @@ try:
     import requests
 except:
     print u"No se encontró el módulo requests. Es necesario descargarlo e instalarlo."
-    print "sudo pip install requests"
-    ret = os.system("sudo pip install requests")
+    print "pip install requests"
+    ret = os.system("pip install requests")
     if ret != 0:
         print u"Ocurrió un error durante la instalación de requests."
         exit(ret)
@@ -137,8 +177,8 @@ for path in os.environ.get('PATH', '').split(':'):
         directorio = path
 if directorio == None:
     print u"No se encontró el Liquidsoap. Es necesario descargarlo e instalarlo."
-    print "sudo apt-get install liquidsoap"
-    ret = os.system("sudo apt-get install liquidsoap")
+    print "apt-get -y install liquidsoap"
+    ret = os.system("apt-get -y install liquidsoap")
     if ret != 0:
         print u"Ocurrió un error durante la instalación de Liquidsoap."
         exit(ret)
@@ -149,21 +189,22 @@ if directorio == None:
 info = None
 GuntherPath = CreateInstallDir() #os.path.join(os.getcwd(), "Gunther")
 print u"Path donde se instalará Günther: " + GuntherPath
-if not os.path.exists(GuntherPath):
-    os.makedirs(GuntherPath)
+if os.path.exists(GuntherPath):
+    if query_si_no("Ya existe el directorio de Günther("+GuntherPath+").\n¿Desea eliminarlo para instalar nuevamente el programa?\nATENCIÓN: ESTA ACCIÓN PUEDE DESHACERSE\nSi tiene información guardada en ese directorio, copiela primero a otro directorio y recién entonces escriba 'si'.","no"):
+        print("Borrando "+GuntherPath+"...")
+        os.system("rm -r \""+GuntherPath+"\"")
+        print(GuntherPath+" borrado con éxito.")
+    else:
+        exit("Instalación cancelada.")
+print("Creando "+GuntherPath+"...")
+os.makedirs(GuntherPath)
 os.chdir(GuntherPath)
 
-try:
-    #repo = git(GuntherPath)
-except Exception as e:
-    svnclient.checkout('svn://svn.code.sf.net/p/radiocefyl/code/trunk/gunther', GuntherPath)
-    info = svnclient.info(GuntherPath)
-
-#Borro cualquier cambio
-svnclient.revert(GuntherPath)
-
-#Actualizo la versión
-svnclient.update(GuntherPath)
+print("Clonando el repositorio de Günther...")
+ret = os.system("git clone https://github.com/Canta/gunther .")
+if ret != 0:
+    print("Error al intentar clonar el repositorio de Günther")
+    exit(ret)
 
 tmp = "#!/usr/bin/env xdg-open\n\n"
 tmp = tmp + "[Desktop Entry]\n"
@@ -180,13 +221,13 @@ f = open(path, "w")
 print>>f, tmp
 f.close()
 
-print "sudo chmod augo+rwx " + path
-os.system("sudo chmod augo+rwx " + path)
-print "sudo chmod -R augo+rw " + GuntherPath
-os.system("sudo chmod -R augo+rw " + GuntherPath)
+print "chmod augo+rwx " + path
+os.system("chmod augo+rwx " + path)
+print "chmod -R augo+rw " + GuntherPath
+os.system("chmod -R augo+rw " + GuntherPath)
 
 #corrijo un problema en la instalación de Liquidsoap: no tiene permisos para escribir en el log.
-print "sudo chmod -R augo+rw /var/log/liquidsoap"
-os.system("sudo chmod -R augo+rw /var/log/liquidsoap")
+print "chmod -R augo+rw /var/log/liquidsoap"
+os.system("chmod -R augo+rw /var/log/liquidsoap")
 
 print u"Instalación finalizada."
